085182cf47d3798d261c67ab6cd5b5629c2d7769
kpgoodale <kpgoodale@gmail.com>
2017-12-12 09:32:58 -0500

    babel-loader and webpack config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ function component() {
   
     // Lodash, imported in this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    let odds = new Array(1, 3, 5);
+    
+    let evens = odds.map(v => v + 1);
+    let pairs = odds.map(v => ({
+        even: v,
+        odd: v + 1
+    }));
+    let nums = evens.map((v, i) => v + i);
+
+    console.log(`Array odds:`, JSON.stringify(odds));
+    console.log(`Array pairs:`, JSON.stringify(pairs));
+    console.log(`Array nums:`, JSON.stringify(nums));
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+      loaders: [
+          {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              query: {
+                  presets: ['env']
+              }
+          }
+      ]
+  },
+  stats: {
+      colors: true
+  },
+  devtool: 'source-map'
 };
